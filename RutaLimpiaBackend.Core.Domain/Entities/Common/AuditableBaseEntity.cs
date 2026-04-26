7609b2a56218741dc32bed900c22b22844bcee3b
Jam1ll <jamilguzman202@gmail.com>
2026-04-26 18:55:10 -0400

    Add core domain entities and auditable base class
    Added CollectionSchedule, Route, Sector, and Truck entities inheriting from AuditableBaseEntity. Introduced AuditableBaseEntity and IEntity for shared properties. Established navigation properties and required fields to support route management domain modeling.

---
+++ RutaLimpiaBackend.Core.Domain/Entities/Common/AuditableBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace RutaLimpiaBackend.Core.Domain.Entities.Common
+{
+    public abstract class AuditableBaseEntity : IEntity
+    {
+        public virtual Guid Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+    }
+}
---
+++ RutaLimpiaBackend.Core.Domain/Entities/Route.cs
@@ -0,0 +1,15 @@
+﻿using RutaLimpiaBackend.Core.Domain.Entities.Common;
+using RutaLimpiaBackend.Core.Domain.Entities.Enums;
+
+namespace RutaLimpiaBackend.Core.Domain.Entities
+{
+    public class Route : AuditableBaseEntity
+    {
+        public Guid IdSector { get; set; }
+        public required string Number { get; set; }
+        public bool IsActive { get; set; }
+
+        //nav props
+        public Sector? Sector { get; set; }
+    }
+}
