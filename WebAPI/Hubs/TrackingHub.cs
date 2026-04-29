b3d0638382ea24d7d557fa06a03f36c92027ec53
Jam1ll <jamilguzman202@gmail.com>
2026-04-28 20:20:27 -0400

    Add SignalR TrackingHub and LocationUpdateDTO
    Added TrackingHub for real-time route tracking using SignalR. Introduced LocationUpdateDTO for location data transfer. Updated WebAPI.csproj to reference Core.Application project.

---
+++ WebAPI/Hubs/TrackingHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RutaLimpiaBackend.Core.Application.DTOs;
+
+namespace WebAPI.Hubs
+{
+    public class TrackingHub : Hub
+    {
+        //CITIZEN 
+        public async Task JoinRouteGroup(string routeId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, routeId);
+        }
+
+        public async Task LeaveRouteGroup(string routeId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, routeId);
+        }
+
+        // COLLECTOR
+        public async Task SendLocationUpdate(LocationUpdateDTO location)
+        {
+            await Clients.Group(location.RouteId).SendAsync("ReceiveLocation", location);
+        }
+    }
+}
