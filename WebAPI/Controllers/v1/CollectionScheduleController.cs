fc9014e7ffaed1e74bdc7d8bdf5eed4fa1b75f0b
eudyyuniorramires <eudyyuniorramires@gmail.com>
2026-05-01 21:40:12 -0400

    Se aplico el controlador de Rutas y Horarios de Rutas

---
+++ WebAPI/Controllers/v1/CollectionScheduleController.cs
@@ -0,0 +1,77 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RutaLimpiaBackend.Core.Application.Features.CollectionScheduleFeatures.Commands.Create;
+using RutaLimpiaBackend.Core.Application.Features.CollectionScheduleFeatures.Commands.Delete;
+using RutaLimpiaBackend.Core.Application.Features.CollectionScheduleFeatures.Commands.Update;
+using RutaLimpiaBackend.Core.Application.Features.CollectionScheduleFeatures.Queries.GetAll;
+using RutaLimpiaBackend.Core.Application.Features.CollectionScheduleFeatures.Queries.GetById;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+
+namespace WebAPI.Controllers.v1
+{
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class CollectionScheduleController : BaseApiController
+    {
+        [HttpPost("Create")]
+        [SwaggerOperation(Summary = "Recibe los datos necesarios para crear un horario de ruta de basura.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(CreateCollectionScheduleCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut("Update")]
+        [SwaggerOperation(Summary = "Recibe los datos necesarios para editar un horario de una ruta de basura.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(UpdateCollectionScheduleCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("Delete")]
+        [SwaggerOperation(Summary = "Recibe el ID correspondiente para eliminar un horario de ruta de basura.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(DeleteCollectionScheduleCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("All")]
+        [SwaggerOperation(Summary = "Obtiene todas los horarios de la rutas de basura con filtros opcionales.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllCollectionSchedulesQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id:guid}")]
+        [SwaggerOperation(Summary = "Obtiene los horarios de la ruta de basura por su ID.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(Guid id, [FromQuery] GetCollectionScheduleByIdQuery query)
+        {
+            query.Id = id;
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
---
+++ WebAPI/Controllers/v1/RouteController.cs
@@ -0,0 +1,77 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RutaLimpiaBackend.Core.Application.Features.RouteFeatures.Commands.Create;
+using RutaLimpiaBackend.Core.Application.Features.RouteFeatures.Commands.Delete;
+using RutaLimpiaBackend.Core.Application.Features.RouteFeatures.Commands.Update;
+using RutaLimpiaBackend.Core.Application.Features.RouteFeatures.Queries.GetAll;
+using RutaLimpiaBackend.Core.Application.Features.RouteFeatures.Queries.GetById;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+
+namespace WebAPI.Controllers.v1
+{
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class RouteController : BaseApiController
+    {
+        [HttpPost("Create")]
+        [SwaggerOperation(Summary = "Recibe los datos necesarios para crear una ruta de basura.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(CreateRouteCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut("Update")]
+        [SwaggerOperation(Summary = "Recibe los datos necesarios para editar una ruta de basura.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(UpdateRouteCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("Delete")]
+        [SwaggerOperation(Summary = "Recibe el ID correspondiente para eliminar una ruta de basura.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(DeleteRouteCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("All")]
+        [SwaggerOperation(Summary = "Obtiene todas las rutas de basura con filtros opcionales.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllRoutesQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id:guid}")]
+        [SwaggerOperation(Summary = "Obtiene una ruta de basura por su ID.")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(Guid id, [FromQuery] GetRouteByIdQuery query)
+        {
+            query.Id = id;
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
